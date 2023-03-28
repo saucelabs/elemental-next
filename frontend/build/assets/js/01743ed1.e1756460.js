61d63423feca963c81ef38aa9348120c6081121c
discombobulateme <paloma.oliveira@saucelabs.com>
2023-03-28 16:32:45 +0200

    Add build

---
+++ frontend/build/assets/js/01743ed1.e1756460.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[858],{4469:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ frontend/build/assets/js/edbf9d4e.397fed00.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[6188],{3002:e=>{e.exports=JSON.parse('{"label":"someone elses grid","permalink":"/docs/tags/someone-elses-grid","allTagsPath":"/docs/tags","count":1,"items":[{"id":"updated-tips/cloud/26-cloud-ruby","title":"Ruby","description":"Intro","permalink":"/docs/updated-tips/cloud/ruby/"}]}')}}]);
