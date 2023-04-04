a88fb085b7231315b1891f9daf8566e1ad46b32a
discombobulateme <paloma.oliveira@saucelabs.com>
2023-04-04 12:39:01 +0200

    Rebuild and fix config

---
+++ frontend/build/assets/js/da9a220d.4ff77024.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[5039],{1730:e=>{e.exports=JSON.parse('{"label":"hola","permalink":"/elemental-next/blog/tags/hola","allTagsPath":"/elemental-next/blog/tags","count":1}')}}]);
