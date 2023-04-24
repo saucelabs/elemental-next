169f0e815bad7bf06e6307e10c09bcd56d58ec15
James Tacker <james.tacker@saucelabs.com>
2023-04-24 16:29:03 -0700

    added getFrontmatter.js

---
+++ frontend/src/scripts/getFrontmatter.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter')
+
+// Set the target directory for the script to read files from
+const targetDir = process.env.DOCS_DIR || 'docs/';
+
+// Function to process the front matter data in a Markdown file
+const processMD = (mdFile) => {
+    // Parse the front matter data using gray-matter library
+    const { data } = matter(fs.readFileSync(mdFile));
+    // Process the tags and categories data from the front matter
+    const tags = data.tags
+        ? data.tags
+            .toString()
+            .split(",")
+            .map((tag) =>
+                tag.trim().replace(/['"]/g, "").replace(/-/g, " ")
+            )
+        : [];
+    const categories = data.category
+        ? data.category
+            .toString()
+            .split(",")
+            .map((cat) =>
+                cat.trim().replace(/['"]/g, "").replace(/-/g, " ")
+            )
+        : [];
+
+    // Create an object containing the processed front matter data
+    const processedData = {
+        title: data.title,
+        id: data.id ? data.id.replace(/"/g, "") : "",
+        slug: data.slug,
+        number: data.number,
+        publish_date: data.publish_date,
+        last_update: data.last_update || "",
+        date: data.date,
+        tags: tags,
+        level: data.level,
+        category: categories,
+    };
+
+    return processedData;
+};
+
+// Function to recursively get front matter data from all Markdown files in a directory
+const getFrontmatter = (dir) => {
+    // Get an array of all files and directories in the given directory
+    const files = fs.readdirSync(dir);
+    // Reduce the array of files and directories to an array of processed front matter data objects
+    return files.reduce((acc, file) => {
+        // Get the full file path and extension
+        const filePath = path.join(dir, file);
+        const ext = path.extname(file);
+        // If the file is a directory, recursively call this function on the directory
+        if (fs.statSync(filePath).isDirectory()) {
+            const nestedFiles = getFrontmatter(filePath);
+            acc.push(...nestedFiles);
+            // If the file is a Markdown file, process its front matter data and add it to the output array
+        } else if (ext === ".md" || ext === ".mdx") {
+            const processedData = processMD(filePath);
+            // Check if the processed front matter data has both a title and an id
+            if (processedData.title && processedData.id) {
+                acc.push(processedData);
+                // If the processed front matter data is missing a title or an id, log a message and skip it
+            } else {
+                console.log(`Skipping ${filePath} because it doesn't have title and/or id frontmatter`);
+            }
+        }
+        return acc;
+    }, []);
+};
+
+// Call the getFrontmatter function to get an array of all processed front matter data objects
+const allData = getFrontmatter(targetDir);
+
+// Write the processed front matter data to a JavaScript file for use in the website
+fs.writeFile(path.join(__dirname, "data.js"), `const Data = ${JSON.stringify(allData, null, 2)};\n\nexport default Data;\n`, (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    // If the script is not running in test mode, log a message indicating that the frontmatter was written to data.js
+    if (process.env.NODE_ENV !== 'test') {
+        console.log(`Frontmatter data written to data.js`);
+    }
+});
+
+module.exports = { getFrontmatter };
