8f5ec25c0a9d59733c9c9c5fe27436b1825ebb16
discombobulateme <paloma.oliveira@saucelabs.com>
2023-05-17 12:23:34 +0200

    Change class menu to dropdown-list
    Class .menu belongs to Docusaurus and it was causing the sidebar to disappear

--- frontend/src/components/dropdown.js
+++ frontend/src/components/dropdown.js
@@ -1,106 +1,85 @@
 import React from 'react';
 import Data from '@site/src/scripts/data';
- 
 
+const Dropdown = ({
+  filterItem,
+  setTip,
+  difficultyItems,
+  tagItems,
+  tagFilters,
+  catItems,
+  catFilters,
+  sortByDate,
+  orderBy,
+}) => {
+  return (
+    <>
+      <div className='dropdown-container'>
+        <p>Filter by:</p>
+        <div className='dropdown'>
+          <button
+            className='drop'
+            onClick={() => {
+              setItem(Data);
+            }}
+          >
+            Difficulty
+          </button>
+          <div className='dropdown-list'>
+            {difficultyItems?.map((Val, id) => {
+              return (
+                <button className='difficulty-list' onClick={() => filterItem(Val)} key={id}>
+                  <div>{Val}</div>
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div className='dropdownTags'>
+          <button className='drop' onClick={() => setItem(Data)}>
+            Tags
+          </button>
+          <div className='dropdown-list'>
+            {tagItems?.map((Val, id) => {
+              return (
+                <button className='content-tags' onClick={() => tagFilters(Val)} key={id}>
+                  <div>{Val}</div>
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div className='dropdownCat'>
+          <button className='drop' onClick={() => setItem(Data)}>
+            Categories
+          </button>
+          <div className='dropdown-list'>
+            {catItems?.map((Val, id) => {
+              return (
+                <button className='cat-list' onClick={() => catFilters(Val)} key={id}>
+                  <div>{Val}</div>
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div className='dropdownDate'>
+          <button className='drop' onClick={() => setItem(Data)}>
+            Order Posted
+          </button>
+          <div className='dropdown-list'>
+            {orderBy?.map((Val, id) => {
+              return (
+                <button className='date-list' onClick={() => sortByDate(Val)} key={id}>
+                  <div>{Val}</div>
+                </button>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
- const Dropdown = ({ filterItem, setTip, difficultyItems ,tagItems, tagFilters, catItems, catFilters, sortByDate, orderBy}) => {
-   return (
-     <>
-     <div className='dropdown-container'>
-      <p>Filter by:</p>  
-       <div className='dropdown'>
-       <button className='drop'
-             onClick={() => {setItem(Data);}}
-             >
-             Difficulty
-       </button>
-         <div className='menu'>
-             {difficultyItems?.map((Val, id) => {
-             return (
-                 <button className='difficulty-list'
-                     onClick={() => filterItem(Val)}
-                     key={id}
-                 >
-                     <div>
-                         {/* <p>Hello World!</p> */}
-                         {Val}
-                     </div>
-                 </button>
-             );
-             })}
-         </div>
-         </div>
-         <div className= 'dropdownTags'>
-         <button className='drop'
-             onClick={() => setItem(Data)}
-             >
-             Tags
-         </button>
-         <div className='menu'>
-             {tagItems?.map((Val, id) => {
-             return (
-                 <button className='content-tags'
-                     onClick={() => tagFilters(Val)}
-                     key={id}
-                 >
-                     <div>
-                         {/* <p>Hello World!</p> */}
-                         {Val}
-                     </div>
-                 </button>
-             );
-             })}
-
-         </div>
-       </div>
-       <div className='dropdownCat'>
-         <button className='drop'
-             onClick={() => setItem(Data)}
-             >
-             Categories
-         </button>
-         <div className='menu'>
-             {catItems?.map((Val, id) => {
-             return (
-                 <button className='cat-list'
-                     onClick={() => catFilters(Val)}
-                     key={id}
-                 >
-                     <div>
-                         {/* <p>Hello World!</p> */}
-                         {Val}
-                     </div>
-                 </button>
-             );
-             })}
-
-         </div>
-       </div>
-       <div className='dropdownDate'>
-         <button className='drop'
-             onClick={() => setItem(Data)}>
-             Order Posted
-         </button>
-         <div className='menu'>
-             {orderBy?.map((Val, id) => {
-             return (
-                 <button className='date-list'
-                     onClick={() => sortByDate(Val)}
-                     key={id}
-                 >
-                     <div>
-                         {/* <p>Hello World!</p> */}
-                         {Val}
-                     </div>
-                 </button>
-             );
-             })}
-
-         </div>
-       </div>
-       </div>
-     </>
-   );
- };
-  
 export default Dropdown;
