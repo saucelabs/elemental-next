91db254cbcaf1fc2520eeadc5455a9d72233964b
discombobulateme <paloma.oliveira@saucelabs.com>
2023-05-20 23:24:05 +0200

    Update dropdown.js

--- frontend/src/components/dropdown.js
+++ frontend/src/components/dropdown.js
@@ -2,17 +2,15 @@ import React from 'react';
 
 const Dropdown = ({ options, selectedOption, onSelectChange }) => {
   return (
-    <>
-      <div className='dropdown'>
-        <select value={selectedOption} onChange={onSelectChange}>
-          {options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-      </div>
-    </>
+    <div className='dropdown'>
+      <select value={selectedOption} onChange={onSelectChange}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
