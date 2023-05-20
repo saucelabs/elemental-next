d3f52be35301feef6ace81e61de8988338832c32
discombobulateme <paloma.oliveira@saucelabs.com>
2023-05-20 23:04:53 +0200

    Change dropdowns logic to allow unselect mode

--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -1,35 +1,36 @@
 import React from 'react';
 
-const Card = ({ tip, tagItems, tagFilters }) => {
-  const Tag = (word) => <button className='tags'>{word}</button>;
-
+const Card = ({ contentUrl, last_update, level, tags, text, title }) => {
   return (
     <div className='cards-container'>
-      {tip.map((Val) => {
-        return (
-          <div className='card' key={Val.id}>
-            <h2>
-              <a href={Val.contentUrl}>{Val.title}</a>
-            </h2>
-            <p> {Val.text}</p>
-            <div className='tags-container'> {Val.tags.map(Tag)} </div>
-            {/* // Self-invoking function */}
-            <div className='level'>
-              {(() => {
-                switch (Val.level) {
-                  case 1:
-                    return <img src={'img/traffic_filter/level1.svg'} alt='Beginner' />;
-                  case 2:
-                    return <img src={'img/traffic_filter/level2.svg'} alt='Intermediate' />;
-                  case 3:
-                    return <img src={'img/traffic_filter/level3.svg'} alt='Advanced' />;
-                }
-              })()}
-            </div>
-          </div>
-        );
-      })}
+      <div className='card'>
+        <h2>
+          <a href={contentUrl}>{title}</a>
+        </h2>
+        <p>{text}</p>
+        {/* Display traffic light = tip difficulty level */}
+        <div className='level'>
+          {(() => {
+            switch (level) {
+              case 1:
+                return <img src={'img/traffic_filter/level1.svg'} alt='Beginner' />;
+              case 2:
+                return <img src={'img/traffic_filter/level2.svg'} alt='Intermediate' />;
+              case 3:
+                return <img src={'img/traffic_filter/level3.svg'} alt='Advanced' />;
+            }
+          })()}
+        </div>
+        {/* Display tags */}
+        <div className='tags-container'>
+          {tags.map((tag) => {
+            return <div className='tags'>{tag + ' '}</div>;
+          })}{' '}
+        </div>
+        <p>{last_update}</p>
+      </div>
     </div>
   );
 };
+
 export default Card;
