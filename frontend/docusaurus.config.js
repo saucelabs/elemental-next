fe3ec844207d3d8078e8e4822697b6694375dcd9
Rajene Harris <45857126+aquaomarine@users.noreply.github.com>
2023-05-21 19:10:12 -0400

    commenting docusaurus config

--- frontend/docusaurus.config.js
+++ frontend/docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    // /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
