ac5ad91b277f29a80b7a2cf7fa479e08f8507dd0
discombobulateme <paloma.oliveira@saucelabs.com>
2023-06-06 18:58:01 +0200

    Fix resources responsivity

--- frontend/src/pages/resources.js
+++ frontend/src/pages/resources.js
@@ -1,110 +1,71 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import selenium from '../../static/img/resource_page/selenium.png';
-import guidebook from '../../static/img/resource_page/guidebook.png';
-import internet from '../../static/img/resource_page/internet.png';
-import github from '../../static/img/resource_page/github.png';
 
-const Resource = () => {
-  return (
-    <div className='resources-container'>
-      <h1>External Resources</h1>
-      <p>
-        The goal of Elemental Selenium is to host information for testers and professionals using Selenium to{' '}
-        <b>stay up to date</b> with the technology they use for their work. However, <b>we can’t cover everything</b>{' '}
-        and never will pretend otherwise.{'\n'}
-      </p>
-      <p>Here’s a collection of external resources that we think are great. </p>
-      <table>
-        <tr>
-          <td>
-            <img src={selenium} />
-          </td>
-          <td></td>
-          <td>
-            <a href='https://www.selenium.dev/documentation/' target='_blank' rel='noopener noreferrer'>
-              Selenium Documentation &#10132;
-            </a>
-            <br />
-            <p>
-              An open source web app perfect for writing tests against, you can find the-internet all over our tips.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <img src={guidebook} />
-          </td>
-          <td></td>
-          <td>
-            <a href='https://seleniumguidebook.com/' target='_blank' rel='noopener noreferrer'>
-              Selenium Guidebook &#10132;
-            </a>
-            <br />
-            <p>
-              An open source web app perfect for writing tests against, you can find the-internet all over our tips.{' '}
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <img src={guidebook} />
-          </td>
-          <td></td>
-          <td>
-            <a href='https://github.com/saucelabs/the-internet/' target='_blank' rel='noopener noreferrer'>
-              The Internet &#10132;
-            </a>
-            <br />
-            <p>
-              An open source web app perfect for writing tests against, you can find the-internet all over our tips.{' '}
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <img src={internet} />
-          </td>
-          <td></td>
-          <td>
-            <a
-              href='https://www.oreilly.com/library/view/hands-on-selenium-webdriver/9781098109998/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Hands-On Selenium WebDriver with Java &#10132;
-            </a>
-            <br />
-            <p>Written by the original author of Elemental Selenium, Dave Haeffner</p>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <img src={github} />
-          </td>
-          <td></td>
-          <td>
-            <a href='https://github.com/bonigarcia/selenium-webdriver-java/' target='_blank' rel='noopener noreferrer'>
-              Examples from Hands-On Selenium WebDriver with Java &#10132;
-            </a>
-            <br />
-            <p>All of the listed examples from Hands-On Selenium WebDriver with Java by Boni Garcia.</p>
-          </td>
-        </tr>
-      </table>
-    </div>
-  );
-};
+const content = [
+  {
+    id: 1,
+    title: 'Selenium Documentation',
+    paragraph: 'An open source web app perfect for writing tests against, you can find the-internet all over our tips.',
+    link: 'https://www.selenium.dev/documentation/',
+    img: 'img/resource_page/selenium.png',
+  },
+  {
+    id: 2,
+    title: 'The Internet',
+    paragraph: 'An open source web app perfect for writing tests against, you can find the-internet all over our tips.',
+    link: 'https://github.com/saucelabs/the-internet/',
+    img: 'img/resource_page/internet.png',
+  },
+  {
+    id: 3,
+    title: 'Hands-On Selenium WebDriver with Java',
+    paragraph: 'Written by the original author of Elemental Selenium, Dave Haeffner',
+    link: 'https://www.oreilly.com/library/view/hands-on-selenium-webdriver/9781098109998/',
+    img: 'img/resource_page/guidebook.png',
+  },
+  {
+    id: 4,
+    title: 'Examples from Hands-On Selenium WebDriver',
+    paragraph: 'All of the listed examples from Hands-On Selenium WebDriver with Java by Boni Garcia.',
+    link: 'https://github.com/bonigarcia/selenium-webdriver-java/',
+    img: 'img/resource_page/github.png',
+  },
+];
 
-export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
+const Resources = () => {
   return (
     <body>
-      <Layout title={`${siteConfig.title}`} description='Description will go into a meta tag in <head />'>
-        <Resource />
-        <img className='resources-footer' src='./img/backgrounds/resources-footer.svg' alt='' />
+      <Layout title='Elemental Selenium Resources' description='External resources for Selenium testers'>
+        <div className='resources-container'>
+          <div className='resources-header'>
+            <h1>External Resources</h1>
+            <p>
+              The goal of Elemental Selenium is to host information for testers and professionals using Selenium to{' '}
+              <b>stay up to date</b> with the technology they use for their work. However,{' '}
+              <b>we can’t cover everything</b> and never will pretend otherwise.{'\n'}
+            </p>
+            <p>Here’s a collection of external resources that we think are great. </p>
+          </div>
+          <div className='resource-content-container'>
+            {content.map((item) => (
+              <div key={item.id} className='content-item'>
+                <img src={item.img} alt='Content Image' className='content-image' />
+                <div className='content-text'>
+                  <h2>
+                    <a href={item.link} target='_blank' rel='noopener noreferrer'>
+                      {item.title} <span>&#10132;</span>
+                    </a>
+                  </h2>
+                  <p className='content-paragraph'>{item.paragraph}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <img className='community-footer' src='img/backgrounds/resources-footer.svg' alt='' />
       </Layout>
     </body>
   );
-}
+};
+
+export default Resources;
