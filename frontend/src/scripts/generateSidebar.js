f7a2f85184c4489875164e34fbc7f9594a835d2e
discombobulateme <paloma.oliveira@saucelabs.com>
2023-06-06 21:11:18 +0200

    Script to generate sidebar

---
+++ frontend/src/scripts/generateSidebar.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const grayMatter = require('gray-matter');
+
+function generateSidebar() {
+  const docsDir = path.resolve(__dirname, '../../docs'); // Update the path to match your project structure
+
+  const sidebar = {
+    docs: [
+      {
+        items: [],
+      },
+    ],
+  };
+
+  function readFiles(dir) {
+    const files = fs.readdirSync(dir);
+
+    files.forEach((file) => {
+      const filePath = path.join(dir, file);
+      const stats = fs.statSync(filePath);
+
+      if (stats.isDirectory()) {
+        readFiles(filePath);
+      } else if (file === 'main.mdx') {
+        const fileContent = fs.readFileSync(filePath, 'utf-8');
+        const { data } = grayMatter(fileContent);
+
+        sidebar.docs[0].items.push(data.title);
+      }
+    });
+  }
+
+  readFiles(docsDir);
+
+  fs.writeFileSync(
+    path.resolve(__dirname, '../../sidebars.js'),
+    `module.exports = ${JSON.stringify(sidebar, null, 2)};`
+  );
+}
+
+generateSidebar();
