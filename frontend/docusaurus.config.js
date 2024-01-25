f7930a80614f4552a2cd531d9334fdf971a6717c
Diego Molina <diemol@gmail.com>
2024-01-25 16:32:24 +0100

    Removing algolia while we sort out the indexes

--- frontend/docusaurus.config.js
+++ frontend/docusaurus.config.js
@@ -211,31 +211,6 @@ const config = {
             colorMode: {
                 disableSwitch: true,
             },
-            algolia: {
-                // The application ID provided by Algolia
-                appId: '7W1XUFVKCS',
-
-                // Public API key: it is safe to commit it
-                apiKey: '00cd55a788fdabb22f72cba843ff820a',
-
-                indexName: 'elementalselenium1',
-
-                // Optional: see doc section below
-                contextualSearch: true,
-
-                // Optional: Specify domains where the navigation should occur through window.location instead on
-                // history.push. Useful when our Algolia config crawls multiple documentation sites, and we want to
-                // navigate with window.location.href to them.
-                externalUrlRegex: 'external\\.com|domain\\.com',
-
-                // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index
-                // for multiple deployments using a different baseUrl. You can use regexp or string in the `from`
-                // param. For example: localhost:3000 vs myCompany.com/docs
-                replaceSearchResultPathname: {
-                    from: '/docs/', // or as RegExp: /\/docs\//
-                    to: '/',
-                },
-            },
         },
 };
 
