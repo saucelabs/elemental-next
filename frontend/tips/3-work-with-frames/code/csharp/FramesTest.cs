51e4d4fe4f59ca272d40288f465b380635ee71d2
Diego Molina <diemol@gmail.com>
2024-02-22 12:26:13 +0100

    Tip 3 in C#, updated

---
+++ frontend/tips/3-work-with-frames/code/csharp/FramesTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace csharp
+{
+    [TestClass]
+    public class FileUploadTest
+    {
+        IWebDriver _driver;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _driver = new FirefoxDriver();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _driver?.Quit();
+        }
+
+        [TestMethod]
+        public void Example1()
+        {
+            _driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/nested_frames");
+            _driver.SwitchTo().Frame(_driver.FindElement(By.Name("frame-top")));
+            _driver.SwitchTo().Frame(_driver.FindElement(By.Name("frame-middle")));
+            Assert.AreEqual("MIDDLE", _driver.FindElement(By.Id("content")).Text);
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            _driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/tinymce");
+            _driver.SwitchTo().Frame(_driver.FindElement(By.Id("mce_0_ifr")));
+            IWebElement Editor = _driver.FindElement(By.Id("tinymce"));
+            string StartText = Editor.Text;
+            Editor.Clear();
+            Editor.SendKeys("Hello World!");
+            string EndText = Editor.Text;
+            Assert.AreNotEqual(EndText, StartText);
+
+            _driver.SwitchTo().DefaultContent();
+            string HeaderText = _driver.FindElement(By.CssSelector("h3")).Text;
+            Assert.AreEqual("An iFrame containing the TinyMCE WYSIWYG Editor", HeaderText);
+        }
+    }
+}
