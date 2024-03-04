19408dd0acd692253f5cec56f6e3c536ec449d6b
Diego Molina <diemol@gmail.com>
2024-03-04 16:29:29 +0100

    Tip 3 in Ruby, updated

---
+++ frontend/tips/3-work-with-frames/code/ruby/frames.rb
@@ -0,0 +1,40 @@
+require 'selenium-webdriver'
+require 'rspec/expectations'
+include RSpec::Matchers
+
+def setup
+  @driver = Selenium::WebDriver.for(:firefox)
+end
+
+def teardown
+  @driver.quit
+end
+
+def run
+  setup
+  yield
+  teardown
+end
+
+run do
+  @driver.get 'https://the-internet.herokuapp.com/nested_frames'
+  frame_top = @driver.find_element(name: 'frame-top')
+  @driver.switch_to.frame(frame_top)
+  frame_middle = @driver.find_element(name: 'frame-middle')
+  @driver.switch_to.frame(frame_middle)
+  expect(@driver.find_element(id: 'content').text).to eql 'MIDDLE'
+end
+
+run do
+  @driver.get 'https://the-internet.herokuapp.com/tinymce'
+  @driver.switch_to.frame(@driver.find_element(id: 'mce_0_ifr'))
+  editor = @driver.find_element(id: 'tinymce')
+  before_text = editor.text
+  editor.clear
+  editor.send_keys 'Hello World!'
+  after_text = editor.text
+  expect(after_text).not_to eql before_text
+
+  @driver.switch_to.default_content
+  expect(@driver.find_element(css: 'h3').text).not_to be_empty
+end
