1cd819b20c244facc253f85e1d5b49a9883c6768
Diego Molina <diemol@gmail.com>
2024-03-19 23:15:57 +0100

    Adjusting Java example tip 5

--- frontend/tips/5-select-from-a-dropdown/code/java/src/test/java/com/elemental/selenium/DropdownTest.java
+++ frontend/tips/5-select-from-a-dropdown/code/java/src/test/java/com/elemental/selenium/DropdownTest.java
@@ -1,8 +1,6 @@
 package com.elemental.selenium;
 
-import java.time.Duration;
 import java.util.List;
-import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
